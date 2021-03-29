a864f1a1c05e30d1cf114804b79b7b71f91072a7
Asaroz <77623721+Asaroz@users.noreply.github.com>
2021-03-29 13:31:15 +0200

    Create java.js

---
+++ java.js
@@ -0,0 +1,7 @@
+let first_name="Lars"
+let last_name="Elberskirch"
+let city="Düsseldorf"
+let street="Teststreet"
+let job="Web-dev"
+
+console.log(first_name,last_name,"is a",job,"from",city,street)
